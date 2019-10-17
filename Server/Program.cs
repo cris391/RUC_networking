800b3351f34223b60943b142d4925676ce77acd5
Cristian <cristianpand@yahoo.com>
2019-10-17 16:21:05 +0200

    client+server: added multithreading capabilities

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,44 +2,76 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-
+using System.Threading;
 namespace Server
 {
   class Program
   {
     static void Main(string[] args)
     {
-      var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5000);
+      new Server(IPAddress.Parse("127.0.0.1"), 5000);
+      Console.WriteLine("Server Started");
+    }
+
+  }
+  class Server
+  {
+    TcpListener server = null;
+    public Server(IPAddress localAddr, int port)
+    {
+      server = new TcpListener(localAddr, port);
       server.Start();
+      StartListener();
+    }
 
-      while (true)
+    public void StartListener()
+    {
+      try
       {
+        while (true)
+        {
+          Console.WriteLine("Waiting for a connection...");
+          var client = server.AcceptTcpClient();
+          Console.WriteLine($"A client connected!");
 
-        var client = server.AcceptTcpClient();
-
-        var stream = client.GetStream();
-
-        var buffer = new byte[client.ReceiveBufferSize];
-
-        var rcnt = stream.Read(buffer, 0, buffer.Length);
-
-        var msg = Encoding.UTF8.GetString(buffer, 0, rcnt);
-
-        if (msg == "exit") break;
-        
-
-        Console.WriteLine($"Message: {msg}");
-
-        msg = msg.ToUpper();
-
-        buffer = Encoding.UTF8.GetBytes(msg);
-
-        stream.Write(buffer, 0, buffer.Length);
-
-        stream.Close();
+          Thread t = new Thread(new ParameterizedThreadStart(HandleDevice));
+          t.Start(client);
+        }
       }
+      catch (SocketException e)
+      {
+        Console.WriteLine("SocketException: {0}", e);
+        server.Stop();
+      }
+    }
 
-      server.Stop();
+    public void HandleDevice(object obj)
+    {
+      TcpClient client = (TcpClient)obj;
+      var stream = client.GetStream();
+
+      var buffer = new byte[client.ReceiveBufferSize];
+      int receiveBufferCount;
+      try
+      {
+        while ((receiveBufferCount = stream.Read(buffer, 0, buffer.Length)) != 0)
+        {
+          var msg = Encoding.UTF8.GetString(buffer, 0, receiveBufferCount);
+          // implement some kind of cleanup if client sends close message(server sid)
+          // if (msg == "exit2") client.Close();
+          Console.WriteLine("{1}: Received: {0}", msg, Thread.CurrentThread.ManagedThreadId);
+
+          string replyMsg = "Hey Device!";
+          var byteReplyMsg = Encoding.UTF8.GetBytes(replyMsg);
+          stream.Write(byteReplyMsg, 0, byteReplyMsg.Length);
+          Console.WriteLine("{1}: Sent: {0}", replyMsg, Thread.CurrentThread.ManagedThreadId);
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("Exception: {0}", e.ToString());
+        client.Close();
+      }
     }
   }
 }
